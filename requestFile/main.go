bc8a7f70f913254549aed9de63ff04f7d7a562d5
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-08-01 10:22:52 -0500

    testing

--- requestFile/main.go
+++ requestFile/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -19,15 +21,23 @@ func main() {
 func UploadFile(w http.ResponseWriter, r *http.Request) {
 	r.ParseMultipartForm(33 << 22)
 
-	buffer := bytes.Buffer
+	var buf bytes.Buffer
 
 	file, header, err := r.FormFile("file")
 	if err != nil {
 		panic(err)
 	}
+	defer file.Close()
 
 	name := strings.Split(header.Filename, ".")
 
 	fmt.Println("Name: ", name)
 
+	f, err := os.OpenFile("./downloaded", os.O_WRONLY|os.O_CREATE, 0666)
+	defer f.Close()
+	io.Copy(f, file)
+
+	contents := buf.String()
+	fmt.Println(contents)
+	buf.Reset()
 }
