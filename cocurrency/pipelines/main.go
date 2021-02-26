49578ced5a7dc04dbe961a5bc530784151f4206c
Christian Hernandez Najera <chrismatla68@gmail.com>
2021-02-25 18:44:35 -0600

    wg

--- cocurrency/pipelines/main.go
+++ cocurrency/pipelines/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 	// Set up the pipeline
@@ -11,10 +14,44 @@ func main() {
 	fmt.Println(<-out)
 	fmt.Println(<-out)
 
-	for n := range sq(sq(gen(1, 2, 3, 4))) {
+	for n := range sq(sq(gen(2, 3))) {
 		fmt.Println(n)
 	}
 
+	// Fan-out, Fan-in
+	in := gen(2, 3)
+
+	// Distribute the sq work across two goroutines
+	c1 := sq(in)
+	c2 := sq(in)
+
+	// Consume the merged output
+	for n := range merge(c1, c2) {
+		fmt.Println(n)
+	}
+}
+
+func merge(cs ...<-chan int) <-chan int {
+	var wg sync.WaitGroup
+	out := make(chan int)
+
+	output := func(c <-chan int) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
 }
 
 func gen(nums ...int) <-chan int {
