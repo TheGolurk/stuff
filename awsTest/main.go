94fc01125d9894b893e541f5f0d19a6f41c289da
Christian Hernandez <chrismatla68@gmail.com>
2021-03-06 22:31:16 -0600

    main

---
+++ awsTest/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+func main() {
+	//	aws ec2 describe-instances --filters "Name=instance-type,Values=t2.micro" --query "Reservations[].Instances[].InstanceId"
+	cmd := exec.Command("aws", "ec2", "describe-instances", "--filters", "Name=instance-type,Values=t2.micro", "--query", "Reservations[].Instances[].InstanceId")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
+
+	var a interface{}
+
+	if err = json.NewDecoder(stdout).Decode(&a); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s", a)
+}
