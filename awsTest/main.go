b448ff1708f5a788f55e7f337e020b8c5ff2691e
Christian Hernandez <chrismatla68@gmail.com>
2021-03-08 15:37:08 -0600

    stuff

--- awsTest/main.go
+++ awsTest/main.go
@@ -1,34 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
 	"os/exec"
 )
 
 func main() {
 	//	aws ec2 describe-instances --filters "Name=instance-type,Values=t2.micro" --query "Reservations[].Instances[].InstanceId"
-	cmd := exec.Command("aws", "ec2", "describe-instances", "--filters", "Name=instance-type,Values=t2.micro", "--query", "Reservations[].Instances[].InstanceId")
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := cmd.Start(); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := cmd.Wait(); err != nil {
-		log.Fatal(err)
-	}
-
-	var a interface{}
-
-	if err = json.NewDecoder(stdout).Decode(&a); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("%s", a)
+	cmd, _ := exec.Command("aws", `ec2 describe-instances --filters "Name=instance-type,Values=t2.micro" --query "Reservations[].Instances[].InstanceId"`).Output()
+	fmt.Println("s", string(cmd))
+	fmt.Printf("%v", cmd)
 
 	// Expecting output with or wihin text (json) -> Then save as json to terminate the instances
 }
