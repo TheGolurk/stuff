83d27ae6ddf1f1ea2771898d1a4546e1ad867b2c
Christian Hernandez <chrismatla68@gmail.com>
2021-05-02 21:24:39 -0500

    Copy pasta fail. still had a instead of a

--- req/main.go
+++ req/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -16,4 +20,18 @@ type UserInfo struct {
 
 func main() {
 
+	User := UserInfo{
+		User:     user,
+		Password: password,
+	}
+
+	UserJson, err := json.Marshal(&User)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	response, err := http.Post("/", "application/json", bytes.NewBuffer(UserJson))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
